a455eca591c49852102d56dc9eed9c5760e52d8a
劉泓奇 <karta0904402henry@gmail.com>
2022-03-09 13:42:36 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace DB5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(77, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名: ";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("新細明體", 12F);
+            this.txtName.Location = new System.Drawing.Point(130, 73);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label2.Location = new System.Drawing.Point(77, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "職稱: ";
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Font = new System.Drawing.Font("新細明體", 12F);
+            this.txtSalary.Location = new System.Drawing.Point(305, 132);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(100, 27);
+            this.txtSalary.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("新細明體", 12F);
+            this.txtPhone.Location = new System.Drawing.Point(305, 73);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 27);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Font = new System.Drawing.Font("新細明體", 12F);
+            this.txtPosition.Location = new System.Drawing.Point(130, 132);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(100, 27);
+            this.txtPosition.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label3.Location = new System.Drawing.Point(252, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "電話: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label4.Location = new System.Drawing.Point(252, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "薪資: ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(70, 185);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(552, 253);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnAdd.Location = new System.Drawing.Point(547, 55);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 27);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "新增";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnSearch.Location = new System.Drawing.Point(547, 22);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 27);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "查詢";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnDel.Location = new System.Drawing.Point(547, 121);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 27);
+            this.btnDel.TabIndex = 11;
+            this.btnDel.Text = "刪除";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnUpdate.Location = new System.Drawing.Point(547, 88);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 27);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "修改";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label5.Location = new System.Drawing.Point(77, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "編號: ";
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Font = new System.Drawing.Font("新細明體", 12F);
+            this.labelID.Location = new System.Drawing.Point(138, 29);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(45, 16);
+            this.labelID.TabIndex = 15;
+            this.labelID.Text = "label6";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnClear.Location = new System.Drawing.Point(547, 154);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 27);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DB5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string cnstr = @"Data Source = (LocalDB)\MSSQLLocalDB;" +
+                       "AttachDbFilename = |DataDirectory|ch18DB.mdf;" +
+                       "Integrated Security = True";
+        void ShowData()
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = cnstr;    
+                SqlDataAdapter daEmplotee = new SqlDataAdapter("SELECT * FROM 員工 ORDER BY 編號 ASC",cn);
+                DataSet ds = new DataSet();
+                daEmplotee.Fill(ds,"員工");
+                dataGridView1.DataSource = ds.Tables["員工"];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelID.Text = "";
+            ShowData();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection())
+                {
+                    cn.ConnectionString = cnstr;
+                    cn.Open();
+                    string sqlStr = $"SELECT * FROM 員工 WHERE 姓名 ='{txtName.Text.Replace("'", "''")}'";
+                    SqlCommand cmd = new SqlCommand(sqlStr, cn);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        labelID.Text = $"{dr["編號"].ToString()}";
+                        txtPosition.Text = $"{dr["職稱"].ToString()}";
+                        txtPhone.Text = $"{dr["電話"].ToString()}";
+                        txtSalary.Text = $"{dr["薪資".ToString()]}";
+                    }
+                    else
+                    {
+                        MessageBox.Show("找不到該名職員");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message+ ",查詢資料發生錯誤");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection())
+                {
+                    cn.ConnectionString = cnstr;
+                    cn.Open();
+                    string sqlStr = $"INSERT INTO 員工(姓名,職稱,電話,薪資) VALUES('{txtName.Text.Replace("'", "''")}'," +
+                        $"'{txtPosition.Text.Replace("'", "''")}','{txtPhone.Text.Replace("'", "''")}',{int.Parse(txtSalary.Text)})";
+                    SqlCommand cmd = new SqlCommand(sqlStr, cn);
+                    cmd.ExecuteNonQuery();
+                }
+                ShowData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message+",新增資料發生錯誤");
+            }
+        }
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using(SqlConnection cn = new SqlConnection())
+                {
+                    cn.ConnectionString= cnstr;
+                    cn.Open();
+                    string sqlStr = $"UPDATE 員工 SET 職稱='{txtPosition.Text.Replace("'", "''")}'," +
+                        $"電話='{txtPhone.Text.Replace("'", "''")}',薪資={int.Parse(txtSalary.Text)} WHERE 姓名='{txtName.Text.Replace("'","''")}'";
+                    SqlCommand cmd = new SqlCommand( sqlStr, cn);
+                    cmd.ExecuteNonQuery();
+                }
+                ShowData();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + ",修改資料發生錯誤");
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = cnstr;
+                cn.Open();
+                string sqlStr = $"DELETE FROM 員工 WHERE 姓名='{txtName.Text.Replace("'", "''")}'";
+                SqlCommand cmd = new SqlCommand(sqlStr, cn);
+                cmd.ExecuteNonQuery();
+            }
+            ShowData();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            labelID.Text = "";
+            txtName.Text = "";
+            txtPosition.Text = "";
+            txtPhone.Text = "";
+            txtSalary.Text = "";
+        }
+    }
+}
